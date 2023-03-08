82ee30f6c6d6f5e0db5ee76e8b4d48c5a1af5c87
dcastrog2uniandes <d.castrog2@uniandes.edu.co>
2023-03-08 07:56:39 -0500

    Se crea estructura del proyecto

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,123 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'alarmas',
+        title: 'Alarmas',
+        type : 'basic',
+        icon : 'heroicons_outline:clock',
+        link : '/Alarmas'
+    },
+    {
+        id   : 'categorias',
+        title: 'Categorías',
+        type : 'basic',
+        icon : 'heroicons_outline:tag',
+        link : '/Categorias'
+    },
+    {
+        id   : 'grupos',
+        title: 'Grupos',
+        type : 'basic',
+        icon : 'heroicons_outline:user-group',
+        link : '/Grupos'
+    },
+    {
+        id   : 'ajustes',
+        title: 'Ajustes',
+        type : 'basic',
+        icon : 'heroicons_outline:cog',
+        link : '/Ajustes'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'alarmas',
+        title: 'Alarmas',
+        type : 'basic',
+        icon : 'heroicons_outline:clock',
+        link : '/alarmas'
+    },
+    {
+        id   : 'categorias',
+        title: 'Categorías',
+        type : 'basic',
+        icon : 'heroicons_outline:tag',
+        link : '/Categorias'
+    },
+    {
+        id   : 'grupos',
+        title: 'Grupos',
+        type : 'basic',
+        icon : 'heroicons_outline:user-group',
+        link : '/Grupos'
+    },
+    {
+        id   : 'ajustes',
+        title: 'Ajustes',
+        type : 'basic',
+        icon : 'heroicons_outline:cog',
+        link : '/Ajustes'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'alarmas',
+        title: 'Alarmas',
+        type : 'basic',
+        icon : 'heroicons_outline:clock',
+        link : '/alarmas'
+    },
+    {
+        id   : 'categorias',
+        title: 'Categorías',
+        type : 'basic',
+        icon : 'heroicons_outline:tag',
+        link : '/Categorias'
+    },
+    {
+        id   : 'grupos',
+        title: 'Grupos',
+        type : 'basic',
+        icon : 'heroicons_outline:user-group',
+        link : '/Grupos'
+    },
+    {
+        id   : 'ajustes',
+        title: 'Ajustes',
+        type : 'basic',
+        icon : 'heroicons_outline:cog',
+        link : '/Ajustes'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'alarmas',
+        title: 'Alarmas',
+        type : 'basic',
+        icon : 'heroicons_outline:clock',
+        link : '/alarmas'
+    },
+    {
+        id   : 'categorias',
+        title: 'Categorías',
+        type : 'basic',
+        icon : 'heroicons_outline:tag',
+        link : '/Categorias'
+    },
+    {
+        id   : 'grupos',
+        title: 'Grupos',
+        type : 'basic',
+        icon : 'heroicons_outline:user-group',
+        link : '/Grupos'
+    },
+    {
+        id   : 'ajustes',
+        title: 'Ajustes',
+        type : 'basic',
+        icon : 'heroicons_outline:cog',
+        link : '/Ajustes'
+    }
+];
