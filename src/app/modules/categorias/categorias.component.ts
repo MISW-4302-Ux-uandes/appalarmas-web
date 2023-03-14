5642524dfe7646a31f132cbd63832f96f06d6db9
nbarreto80235 <nbarreto@uniandes.edu.co>
2023-03-14 13:34:43 -0500

    Categorias

--- src/app/modules/categorias/categorias.component.ts
+++ src/app/modules/categorias/categorias.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-categorias',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./categorias.component.scss']
 })
 export class CategoriasComponent {
+  panelOpenState = false;
+
+
+    constructor(private _matDialog: MatDialog,){}
+
+    abrirFormCrearGrupo(){
+    /*
+    const dialogRef = this._matDialog.open(GroupFormComponent);
+
+    dialogRef.afterClosed()
+                .subscribe((result) => {
+                    console.log('Compose dialog was closed!');
+                });
+                */
+    }
 
 }
